6611e3c414f1e4d8e2ac304900a83074fc971863
ccq_y7000 <chucongqing@gmail.com>
2020-11-14 00:37:51 +0800

    finish triangle

--- LearnOpenGL/LOpenGL/main.cpp
+++ LearnOpenGL/LOpenGL/main.cpp
@@ -17,6 +17,7 @@ void processInput(GLFWwindow* window) {
 
 void Render();
 void RenderInit();
+void Shaders();
 
 int main() {
 	glfwInit();
@@ -66,22 +67,23 @@ const char* vertexShaderSource = "#version 330 core\n"
 "}\0";
 
 
-const char* fragmentShaderSource = "#version 330 core \n"
+const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\0";
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
-uint vertexShader;
-uint fragmentShader;
+
+//uint vertexShader;
+//uint fragmentShader;
 
 uint shaderProgram;
-uint VBO, VAO;
+uint VBO, VAO, EBO;
 
 void Shaders() {
 
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+ uint	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 
@@ -95,8 +97,9 @@ void Shaders() {
 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	uint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 	if (!success)
 	{
@@ -122,38 +125,58 @@ void Shaders() {
 
 }
 
-void RerderInit() {
+//float vertices[] = {
+//-0.5f, -0.5f, 0.0f,
+// 0.5f, -0.5f, 0.0f,
+// 0.0f,  0.5f, 0.0f
+//};
+
+float vertices[] = {
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+};
+unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+};
+
+void RenderInit() {
 	glGenBuffers(1, &VBO);
-
+	glGenBuffers(1, &EBO);
 	glGenVertexArrays(1, &VAO);
-}
-void Render() {
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT );
-
-
-	//Vector3 vertices[] = {
-	//	Vector3(-0.5, -0.5, 0.0),
-	//	Vector3(0.5, -0.5, 0.0),
-	//	Vector3(0.0, 0.5f, 0.0f)
-	//};
-
-	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
-	};
-
-
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	//是否要进行线框渲染
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+}
+void Render() {
+	glClearColor(0.5f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
 	glUseProgram(shaderProgram);
 	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 }
