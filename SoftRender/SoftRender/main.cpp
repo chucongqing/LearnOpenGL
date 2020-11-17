641e4910ac61f73c1a0349e6ea2670ad10c4e000
ccq_y7000 <chucongqing@gmail.com>
2020-11-18 00:25:39 +0800

    ah

--- SoftRender/SoftRender/main.cpp
+++ SoftRender/SoftRender/main.cpp
@@ -7,10 +7,10 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
-
+const TGAColor green = TGAColor(0, 255, 0, 255);
 Model* model = NULL;
-const int width = 800;
-const int height = 800;
+const int width = 200;
+const int height = 200;
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 
@@ -43,17 +43,54 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 		}
 	}
 }
-int main(int argc, char** argv) {
 
-	if (2 == argc) {
-		model = new Model(argv[1]);
+void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
+	bool steep = false;
+	if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+		std::swap(p0.x, p0.y);
+		std::swap(p1.x, p1.y);
+		steep = true;
 	}
-	else {
-		model = new Model("obj/african_head.obj");
+	if (p0.x > p1.x) {
+		std::swap(p0, p1);
 	}
 
-	TGAImage image(width, height, TGAImage::RGB);
+	for (int x = p0.x; x <= p1.x; x++) {
+		float t = (x - p0.x) / (float)(p1.x - p0.x);
+		int y = p0.y * (1. - t) + p1.y * t;
+		if (steep) {
+			image.set(y, x, color);
+		}
+		else {
+			image.set(x, y, color);
+		}
+	}
+}
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+
+	if (t0.y == t1.y && t0.y == t2.y) return; // I dont care about degenerate triangles 
+   // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+	if (t0.y > t1.y) std::swap(t0, t1);
+	if (t0.y > t2.y) std::swap(t0, t2);
+	if (t1.y > t2.y) std::swap(t1, t2);
+	int total_height = t2.y - t0.y;
+	for (int i = 0; i < total_height; i++) {
+		bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+		int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+		float alpha = (float)i / total_height;
+		float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here 
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+		if (A.x > B.x) std::swap(A, B);
+		for (int j = A.x; j <= B.x; j++) {
+			image.set(j, t0.y + i, color); // attention, due to int casts t0.y+i != A.y 
+		}
+	}
+}
+
+
+void facerender(TGAImage &image) {
 	for (int i = 0; i < model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
 		for (int j = 0; j < 3; j++) {
@@ -66,9 +103,26 @@ int main(int argc, char** argv) {
 			line(x0, y0, x1, y1, image, white);
 		}
 	}
+}
+int main(int argc, char** argv) {
+
+	if (2 == argc) {
+		model = new Model(argv[1]);
+	}
+	else {
+		model = new Model("obj/african_head.obj");
+	}
+
+	TGAImage image(width, height, TGAImage::RGB);
 
 
-	//like opengl left bottom corner is 0,0
+	Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
+	Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
+	Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+	triangle(t0[0], t0[1], t0[2], image, red);
+	triangle(t1[0], t1[1], t1[2], image, white);
+	triangle(t2[0], t2[1], t2[2], image, green);
+	// like opengl left bottom corner is 0, 0
 	image.flip_vertically(); 
 	image.write_tga_file("output.tga");
 	return 0;
